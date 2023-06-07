4a6fee0302e7988ff83ba2d0ed42998f5169b0fa
prashant <63295541+Prashant0313@users.noreply.github.com>
2023-06-07 16:49:03 +0530

    Update myapp.js

--- myapp.js
+++ myapp.js
@@ -3,13 +3,18 @@ const os = require('os');
 
 const listenPort = 8080;
 
-console.log('Server starting on host '+os.hostname()+' port '+listenPort+'...');
+server_name = 'Generic Server';
+if ('SERVER_NAME' in process.env) {
+  server_name = process.env.SERVER_NAME;
+}
+
+console.log(server_name+' v1 starting on host '+os.hostname()+' port '+listenPort+'...');
 
 const server = http.createServer((req, res) => {
   let clientIP = req.connection.remoteAddress;
   console.log('Processing request for '+req.url+' from '+clientIP);
   res.writeHead(200);
-  res.end('Hello '+clientIP+', this is '+os.hostname()+'\n');
+  res.end('Hello '+clientIP+', this is '+server_name+' v1 on '+os.hostname()+'\n');
 });
 
 server.listen(listenPort);
